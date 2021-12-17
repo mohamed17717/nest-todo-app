a9b2c571f83c5bfc36019d30b7f06ce991ea15a9
Mo17 <mo.err17@gmail.com>
2021-12-17 19:03:54 +0200

    connect to postgesql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
-import { ItemsController } from './items/items.controller';
-import { ItemsService } from './items/items.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule],
-  controllers: [AppController, ItemsController],
-  providers: [AppService, ItemsService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      name: 'default',
+      type: 'postgres',
+      url: process.env.POSTGRES_URL,
+      synchronize: true, // not in production
+      logging: true,
+      entities: ['src/entity/*.*'],
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
